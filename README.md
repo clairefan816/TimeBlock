900ab707042cf3874f6c07c7c28c713c8b2e1a7d
clairefan816 <clairefan816@gmail.com>
2021-03-15 14:42:09 -0700

    add tasks and colors

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ March 14 2021 1 hour
 3) Create basic grid layout
 
 March 15 2021 1.5 hours
-After several trys, finally nail down the data structure of the calendar to be 2-D array.
+1) After several trys, finally nail down the data structure of the calendar to be 2-D array.
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 import './Task.css'
 
 class Task extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Tasks : ["Sleep", "Reading", "Sleep", "Reading","Sleep", "Reading"],
+            Colors: ["Black", "Red", "Orange", "Yellow", "Blue", "Pink"]
+        }
+    }
     render(){
         return(
         <div className="Task">
             <div className="TaskContent">
-                I am the Task Component
+                {this.state.Tasks.map((task, index)=>{
+                    let str = this.state.Colors[index]
+                    return (
+                        <div className="Task-Block" style={{backgroundColor:str}}>
+                            {task}
+                        </div>
+                    )
+                })}
             </div>
             
         </div>
