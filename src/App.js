2174954469adba231da8f4fe09333b4a2a9798c1
Minghao Shan <minghao8shan@gmail.com>
2021-03-29 23:07:12 -0700

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import DayContainer from './containers/DayContainer'
 import TaskContainer from './containers/TaskContainer'
 import Header from './components/Header/Header'
 import Manual from './components/Manual/Manual'
 import Landing from './components/Landing/Landing'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import NotFound from "./containers/NotFound";
+import Login from './containers/Login'
+// import Signup from "./containers/Signup";
+import NotFound from "./containers/NotFound"
 
 
 function App() {
@@ -20,9 +22,9 @@ function App() {
             <Route exact path='/landing' component={Landing} />
             <Route exact path='/time' render={()=>{return <div className="MainContent"><DayContainer /><TaskContainer /></div>}}/>
             <Route exact path='/manual' component={Manual} />
-            <Route>
-                <NotFound />
-            </Route>
+            <Route exact path="/login" component={Login} />
+            {/* <Route exact path="/signup" component={Signup} /> */}
+            <Route component={NotFound} />
           </Switch>
       </Router>
 
